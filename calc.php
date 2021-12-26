9e0052c3f8b6fedefb08c0b0ff43a923091e217d
TaniyaHaghighi20 <81079805+TaniyaHaghighi20@users.noreply.github.com>
2021-12-26 14:38:10 +0330

    php file for calculating the entered expression and generating dynamic page for result

---
+++ calc.php
@@ -0,0 +1,32 @@
+<?php
+$exp = $_GET["expression"];
+//echo $exp;
+if($exp==""){
+    echo "empty input!";
+}else
+if(preg_match('~^[0-9+\-*\/]+$~', $exp)){
+    try {
+        $p = eval('return '.$exp.';');
+        echo $exp;
+        echo "<br>";
+        echo "the result is: ";
+        echo $p;
+    }catch (ParseError $e){
+        echo "wrong way of writing expression <br>";
+        echo "example of expression: 100 + 25*2";
+    }
+
+//    while(preg_match('~^[*\/]+$~', $exp)){
+//        $divIndex = stripos($exp,"/");
+//        $mulIndex = stripos($exp,"*");
+//        if($divIndex<$mulIndex){
+//
+//        }
+//    }
+} else {
+    echo "Bad Expression!";
+}
+echo "<br>";
+?>
+
+<a href="http://localhost:8000/">Go Back</a>
